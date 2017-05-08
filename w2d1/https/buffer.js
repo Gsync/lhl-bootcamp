79de0f6b60b09e2501714650fc28142583bd48a1
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-05-08 18:48:33 +0000

    step 2 of http exercise

---
+++ w2d1/https/buffer.js
@@ -0,0 +1,36 @@
+////###HTTPS buffer EXERCISE
+
+var https = require('https');
+var data = '';
+
+function getAndPrintHTML() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  }
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      for (i=0; i<data.length; i++) {
+        data.push(chunk[i]);
+      }
+      console.log('Chunk Received. Length:', data.length);
+    });
+
+
+    response.on('data', function(data) {
+      console.log(data);
+
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+getAndPrintHTML();
